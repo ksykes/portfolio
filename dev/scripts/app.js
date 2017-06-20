2e571dc34c61451afc0bf0ef509e4a8a037666da
Kait Sykes <kait@kaitsykes.com>
2017-06-20 10:05:23 -0400

    responsive styling

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
 
+	// scroll nav bar
 	$(window).on('scroll', function() {
 		var headerHeight = $('header').outerHeight();
 		// var navHeight = $('nav').outerHeight();
 			if ($(window).scrollTop() >= headerHeight) {
+				// $('body').css('padding-top', '26px');
 				$('nav').addClass('fixed');
 			}
 			else {
 				$('nav').removeClass('fixed');
+				// $('body').css("padding-top",'0');
 			}
+	});
+
+	// hamburger links
+	$( ".hamburgerLinks" ).hide();
+
+	$( ".hamburger" ).click(function() {
+		$( ".hamburgerLinks" ).slideToggle( "slow" );
+		$( ".fa-bars" ).toggleClass( "turnHamburger" );
 	});
 });
