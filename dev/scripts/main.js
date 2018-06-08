d1c2d26043912521ade9d074048126f1eb6eca4a
ksykes <kaitlyn.sykes@me.com>
2018-06-07 21:07:43 -0400

    updated project descriptions

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -2,3 +2,10 @@
 particlesJS.load('particles-js', 'assets/particles-config.json', function () {
     console.log('callback - particles.js config loaded');
 });
+
+(function() {
+    var header = document.getElementById("header-social");
+    document.getElementById("content").onscroll = function scrollFunction() {
+        header.className = "hide";
+    };
+})();
