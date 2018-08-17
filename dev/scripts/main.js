25288b19168070c846203cebbb046c91eb9279e7
ksykes <kait@kaitsykes.com>
2018-08-17 10:14:56 -0400

    refactored js

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,13 +1,18 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+'use strict';
+
+// load particlesJS library
 particlesJS.load('particles-js', 'assets/particles-config.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
-(function() {
+// events to trigger on page scroll
+const scrollEvents = () => {
     var header = document.getElementById("header-social");
     var arrow = document.getElementById("arrow");
     document.getElementById("content").onscroll = function scrollFunction() {
         arrow.className = "hide";
         header.className = "hide";
     };
-})();
+};
+
+scrollEvents();
