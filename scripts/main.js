218c020e0b65b8001c6aa3b11260dd66bbebbde5
Kait <sykeskaitlyn@gmail.com>
2021-04-27 22:03:41 -0400

    bug fix from 594cf799dbc4321e249ba08dfd511a6cf1a4ae99

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
 // load particlesJS library
-particlesJS.load('particles-js', 'assets/particles-config.json', function () {
+particlesJS.load('particles-js', 'particles/particles-config.json', function () {
     console.log('callback - particles.js config loaded');
 });
 
 // events to trigger on page scroll
 const scrollEvents = () => {
-    var header = document.getElementById("header-social");
+    // var header = document.getElementById("header-social");
     var arrow = document.getElementById("arrow");
     document.getElementById("content").onscroll = function scrollFunction() {
         arrow.className = "hide";
-        header.className = "hide";
+        // header.className = "hide";
     };
 };
 
